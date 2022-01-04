dabff0e754acb705daab28015785d6238e57f11a
Sanchit Kalra <sanchit.kalra1@gmail.com>
2022-01-04 12:39:00 +0530

    Initial Commit

---
+++ Timekeeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timekeeper
+//
+//  Created by Mohinder Kalra on 04/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timekeeper/TimekeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimekeeperApp.swift
+//  Timekeeper
+//
+//  Created by Mohinder Kalra on 04/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TimekeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
