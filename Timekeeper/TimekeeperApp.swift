2c2eec408c6bc7b0434a05b51efacd1845e18714
Sanchit Kalra <sanchit.kalra1@gmail.com>
2022-01-05 18:48:32 +0530

    Force app to use light mode

--- Timekeeper/TimekeeperApp.swift
+++ Timekeeper/TimekeeperApp.swift
@@ -12,6 +12,7 @@ struct TimekeeperApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.light)
         }
     }
 }
---
+++ Timekeeper/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Timekeeper
+//
+//  Created by Mohinder Kalra on 04/01/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
